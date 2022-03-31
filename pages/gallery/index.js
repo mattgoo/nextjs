68d46dcdcf0729b5ccdad4c5666d30176a846688
Matt Goodwin <118goodwin@gmail.com>
2022-03-31 08:58:48 -0400

    push

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -5,7 +5,7 @@ import styles from '../../styles/Home.module.css'
 export default function Home() {
     return (
         <div className={styles.container}>
-            <h1 className="title">
+            <h1 className={styles.title}>
                 Welcome to the Gallery!
             </h1>
             <Image src="/images/chopperHat.png" alt="chopperHat" width={200} height={200} />
