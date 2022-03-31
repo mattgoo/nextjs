b9c59b1d48a57e681f7c846cc4a065813b79fc17
Matt Goodwin <118goodwin@gmail.com>
2022-03-31 08:34:46 -0400

    push

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -3,8 +3,8 @@ import Link from 'next/link'
 
 export default function Home() {
     return (
-        <div className="container">
-            <h1 style={{alignContent: 'center', justifyContent:'center'}}>Welcome To The Gallery</h1>
+        <div style={{alignContent: 'center', justifyContent:'center'}}>
+            <h1>Welcome To The Gallery</h1>
         </div>          
     )
 }
