f95716f4fcbca16e5ad60d93138342cbc2b17cdf
Matt Goodwin <118goodwin@gmail.com>
2022-03-31 07:21:03 -0400

    added route to second page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import link from 'next/link'
+import Link from 'next/link'
 
 export default function Home() {
   return (
