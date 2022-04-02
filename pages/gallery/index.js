fefcf807a13368ed6f769e24f7a47d1c26429143
Matt Goodwin <118goodwin@gmail.com>
2022-04-02 15:40:42 -0400

    added a script

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,7 +1,8 @@
 import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
-import Head from 'next/head'
-import Link from 'next/link'
+import Head from 'next/head' //Changes the tab title
+import Link from 'next/link' //Link to another page
+import Script from 'next/script' //Allows you to add scripts to the page
 
 
 export default function Home() {
@@ -10,11 +11,27 @@ export default function Home() {
             <Head>
                 <title>Gallery!</title>
             </Head>
+
+            <Script
+                src="https://connect.facebook.net/en_US/sdk.js"
+                strategy="lazyOnload"
+                onLoad={() =>
+                    console.log('Facebook SDK loaded')
+                }
+            />
+
+
+
             <h1>
                 <Link href="/">
                     <a>Go Home!</a>
                 </Link>
             </h1>
+
+
+
+
+
             <Image src="/images/chopperHat.png" alt="chopperHat" width={200} height={200} />
         </div>      
     )
