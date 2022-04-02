fb79f1172bb6533f8542b451fcb8062ba60f2948
Matt Goodwin <118goodwin@gmail.com>
2022-04-02 15:33:35 -0400

    added Head card

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,12 +1,19 @@
 import Image from 'next/image'
 import styles from '../../styles/Home.module.css'
+import Head from 'next/head'
+import Link from 'next/link'
 
 
 export default function Home() {
     return (
         <div className={styles.container}>
-            <h1 className={styles.title}>
-                Welcome to the Gallery!
+            <Head>
+                <title>Gallery!</title>
+            </Head>
+            <h1>
+                <Link href="/">
+                    <a>Go Home!</a>
+                </Link>
             </h1>
             <Image src="/images/chopperHat.png" alt="chopperHat" width={200} height={200} />
         </div>      
