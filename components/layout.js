c152f2059b3e0aaf5c79848aeaafa78e0dc13c70
Matt Goodwin <118goodwin@gmail.com>
2022-04-02 16:12:20 -0400

    added my name

--- components/layout.js
+++ components/layout.js
@@ -15,7 +15,7 @@ import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = 'Your Name'
+const name = 'Matt Goodwin'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
