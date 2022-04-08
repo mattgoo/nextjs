c2be0a14274f63917878c223ba4aec9294d6b3f3
Matt Goodwin <118goodwin@gmail.com>
2022-04-08 12:47:36 -0400

    error

--- pages/info/index.js
+++ pages/info/index.js
@@ -4,11 +4,8 @@ import styles from '../../styles/myStyles.css'
 
 export default function Home() {
     return (
-        // three equal size divs separated by a black vertical line
+        // three equal size divs separated by a black vertical line 
         <div className={styles.container}>
-            <div className={styles.Info}></div>
-            <div className={styles.Gallery}></div>
-            <div className={styles.Projects}></div>
         </div>
     )
 }
