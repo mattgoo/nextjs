2f9b410085f01ffcc6525dc3294ba68cb9d9766a
Matt Goodwin <118goodwin@gmail.com>
2022-04-08 12:49:58 -0400

    edit

--- pages/info/index.js
+++ pages/info/index.js
@@ -1,11 +1,33 @@
 import Link from 'next/link'
-import styles from '../../styles/myStyles.css'
 
 
 export default function Home() {
     return (
         // three equal size divs separated by a black vertical line 
-        <div className={styles.container}>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-4">
+                    <h1>
+                        <Link href="/">
+                            <a>Go Home!</a>
+                        </Link>
+                    </h1>
+                </div>
+                <div className="col-md-4">
+                    <h1>
+                        <Link href="/info">
+                            <a>Go Info!</a>
+                        </Link>
+                    </h1>
+                </div>
+                <div className="col-md-4">
+                    <h1>
+                        <Link href="/gallery">
+                            <a>Go Gallery!</a>
+                        </Link>
+                    </h1>
+                </div>
+            </div>
         </div>
     )
 }
